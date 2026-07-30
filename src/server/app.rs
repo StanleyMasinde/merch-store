2db50ec4d622f909414e70cd007ac0b1e0dc377a
Stanley Masinde <stanmasinde@gmail.com>
2026-07-30 20:58:59 +0300

    make the app run in the local loopback and print the address

--- src/server/app.rs
+++ src/server/app.rs
@@ -2,7 +2,8 @@ use axum::{Router, routing::get};
 
 pub async fn start_server() {
     let app = Router::new().route("/", get(index_handler));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    println!("App running on http://localhost:3000");
     axum::serve(listener, app).await.unwrap();
 }
 
